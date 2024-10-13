643cd64979491eb494ca366ab8ea7247a0975f2c
owyu <owyu@DESKTOP-QN8EIDM>
2024-10-13 13:54:25 +0300

    ///

--- labs/2.1/main.cpp
+++ labs/2.1/main.cpp
@@ -1,70 +1,143 @@
+/**
+ * Done by:
+ * Student Name: Malinovskyi Vlad
+ * Student Group: 121
+ * Lab 2.1
+ **/
+
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
 
 using namespace std;
 
-// Function for bubble sort with the ability to choose the sorting direction
-void bubbleSort(int arr[], int n, bool ascending = true) {
-    for (int i = 0; i < n - 1; i++) {
-        for (int j = 0; j < n - i - 1; j++) {
-            // If ascending mode, sort in ascending order
-            if (ascending) {
-                if (arr[j] > arr[j + 1]) {
-                    // Change places if elements are not in the correct order
-                    int temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
-                }
-            }
-            // If descending mode, sort in descending order
-            else {
-                if (arr[j] < arr[j + 1]) {
-                    // Change places if elements are not in the correct order
-                    int temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
-                }
+int main() {
+
+    int el[7];
+
+    for (int i = 0; i < 7; i++) {
+        if (i % 2 == 0) {
+            el[i] = i * i + 1;
+        }
+        else {
+            el[i] = i - 8;
+        }
+    }
+
+    int size = 7;
+
+    for (int i = 0; i < size - 1; i++) {
+        for (int k = 0; k < size - i - 1; k++) {
+            if (el[k] > el[k + 1]) {
+                int temp = el[k];
+                el[k] = el[k + 1];
+                el[k + 1] = temp;
             }
         }
     }
-}
 
-// Function to print the array
-void printArray(int arr[], int n) {
-    for (int i = 0; i < n; i++) {
-        cout << arr[i] << " ";
+    cout << "Sorted array: ";
+    for (int i = 0; i < size; i++) {
+        cout << el[i] << endl;
     }
     cout << endl;
-}
-
-int main() {
-    srand(static_cast<unsigned>(time(0)));
-
-    int n = 10;
-    int arr[10]; //0, 1, 2, 3
-    int arr2[4] = {3, 5, 8, 9};
-
-    for (int i = 0; i < n; i++) {
-        cout << " " << arr[i];
-        arr[i] = rand() % 100;
-    }
-    cout << ' ' << endl;
-
-    cout << "Initial array: ";
-    printArray(arr, n);
-
-    // Sorting in ascending order
-    cout << "Sorting in ascending order: ";
-    bubbleSort(arr, n); // true - ascending
-    printArray(arr, n);
-
-    // Sorting in descending order
-    cout << "Sorting in descending order: ";
-    bubbleSort(arr, n, false); // false - descending
-    printArray(arr, n);
 
     return 0;
 }
 
+int arr1[10], arr2[10], arr3[10];
 
+int main() {
+
+    for (int i = 0; i < 10; i++) {
+        arr1[i] = 117 + i;
+    }
+
+    for (int i = 0; i < 10; i++) {
+        arr2[i] = 127 - 2 * i;
+    }
+
+    int k = 0;
+    for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 10; j++) {
+            if (arr1[i] == arr2[j]) {
+                arr3[k] = arr1[i];
+                k++;
+                break;
+            }
+        }
+    }
+
+    if (k >= 2) {
+        int min1 = arr3[0];
+        int min2 = arr3[1];
+
+        if (min1 > min2) {
+            swap(min1, min2);
+        }
+
+        for (int i = 2; i < k; i++) {
+            if (arr3[i] < min1) {
+                min2 = min1;
+                min1 = arr3[i];
+            }
+            else if (arr3[i] < min2) {
+                min2 = arr3[i];
+            }
+        }
+
+
+        int s = min1 + min2;
+        cout << "The sum of two minimal elements: " << s << endl;
+    }
+    else {
+        cout << "There are not enough elements in the array to find the two minimum values." << endl;
+    }
+
+    return 0;
+}
+
+int main() {
+
+    int arr[4][4] = {
+        {1, 2, 3, 4},
+        {5, 6, 7, 8},
+        {9, 10, 11, 12},
+        {13, 14, 15, 16}
+    };
+
+    int arr1[4];
+    for (int i = 0; i < 4; i++) {
+        arr1[i] = arr[i][i];
+    }
+
+    cout << "Elements of the main diagonal 4x4: " << endl;
+    for (int i = 0; i < 4; i++) {
+        cout << arr1[i] << " ";
+    }
+    cout << endl;
+
+    double arr2[4][5] = {
+        {1.1, 2.2, 3.3, 4.4, 5.5},
+        {6.6, 7.7, 8.8, 9.9, 10.0},
+        {11.1, 12.2, 13.3, 14.4, 15.5},
+        {16.6, 17.7, 18.8, 19.9, 20.0}
+    };
+
+    double avgArr[4];
+    for (int i = 0; i < 4; i++) {
+        double sum = 0;
+        for (int j = 0; j < 5; j++) {
+            sum += arr2[i][j];
+        }
+        avgArr[i] = sum / 5.0;
+    }
+
+    cout << "The average arithmetic value of the elements of the 4x5 rows: " << endl;
+    for (int i = 0; i < 4; i++) {
+        cout << fixed << setprecision(2) << avgArr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
--- practicals/2.1/main.cpp
+++ practicals/2.1/main.cpp
@@ -1,59 +1,47 @@
-#include <iostream>
-#include <vector>
-#include <cstdlib>  // For rand() and srand()
-#include <ctime>    // For initializing rand() through time()
-#include <iomanip>  // For std::setw
+/**
+ * Done by:
+ * Student Name: Malinovskyi Vlad
+ * Student Group: 121
+ * Prac 2.1
+ **/
 
-// matrix in comment 10x10 with random numbers from -100 to 100
-// Generated sequences:
-// Sequence  1: -77  -9  46  19  44  21 -94  22  26 -99 
-// Sequence  2: -83 -10  32  93  98   6  77  61  43 -26 
-// Sequence  3: -96  37 -28  75 -80  26  13  -2 -53 -73 
-// Sequence  4: -92   9 -85 -15 -10 -42   1 -57 -21  76 
-// Sequence  5:  88   2  51 -87 -29  12  50  88 -81  -5 
-// Sequence  6:  80   0  30 -12 -85  26 -68   8 -91 -72 
-// Sequence  7:  55 -32 -69 -38  79  89  82 -10  14 -56 
-// Sequence  8: -82 -49  17 -28  71 -79  72  53  -1 -53 
-// Sequence  9: -79 -71 -92  -1  35 -86  90  -8 -15 -34 
-// Sequence 10: -97  35  27   9 -87  40   3 -65  66  45 
+#include <iostream>
+using namespace std;
 
 int main() {
-    srand(static_cast<unsigned>(time(0)));  // Initialize random number generator
+    int A[3][4] = { {3, 5, 2, -8}, {7, -1, 4, 10}, {9, 0, -6, 11} };
 
-    int m = 10;  // Number of sequences
-    int n = 10; // Length of each sequence
-
-    std::vector<std::vector<int>> A(m);  // Set of sequences
-
-    // Generation of random numbers for each sequence
-    for (int i = 0; i < m; i++) {
-        A[i].resize(n);
-        for (int j = 0; j < n; j++) {
-            A[i][j] = (rand() % 199) - 99;  // Random numbers from -99 to 99
-        }
-    }
-
-    // Search for the maximum value by absolute value
-    int max_value = A[0][0];  // Initialize with the first element
-    for (const std::vector<int>& sequence : A) {
-        for (int value : sequence) {
-            if (std::abs(value) > std::abs(max_value)) {
-                max_value = value;
+    int minValue = A[0][0], minRow = 0, minCol = 0;
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 4; j++) {
+            if (A[i][j] < minValue) {
+                minValue = A[i][j];
+                minRow = i;
+                minCol = j;
             }
         }
     }
+    cout << "Minimum value: " << minValue << " at position [" << minRow << "][" << minCol << "]\n";
 
-    // Output results
-    std::cout << "Generated sequences:\n";
-    for (int i = 0; i < m; i++) {
-        std::cout << "Sequence " << std::setw(2) << i + 1 << ": ";
-        for (int value : A[i]) {
-            std::cout << std::setw(3) << value << " ";
+    int maxNegative = -1000;
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 4; j++) {
+            if (A[i][j] < 0 && A[i][j] > maxNegative) {
+                maxNegative = A[i][j];
+            }
         }
-        std::cout << std::endl;
     }
+    cout << "Maximum negative value: " << maxNegative << "\n";
 
-    std::cout << "\nThe maximum value by absolute value from all sequences: " << max_value << std::endl;
+    int minPositive = 1000;
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 4; j++) {
+            if (A[i][j] > 0 && A[i][j] < minPositive) {
+                minPositive = A[i][j];
+            }
+        }
+    }
+    cout << "Minimum positive value: " << minPositive << "\n";
 
     return 0;
 }
